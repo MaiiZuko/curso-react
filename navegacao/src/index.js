a66de9d1a77a6ea5d65f37599940d1bbfdca4dfd
MaiiZuko <maiaramzucco@gmail.com>
2024-02-29 10:11:33 -0300

    update

--- navegacao/src/index.js
+++ navegacao/src/index.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import './index.css'
 import ReactDOM from 'react-dom'
+import React from 'react'
+
+import App from './views/App'
 
 ReactDOM.render(
-    <h1>App</h1>,
+    <App />,
     document.getElementById('root')
 )
---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = props => {
+    <div className="About">
+        Componente About
+        <h1>SObre</h1>
+    </div>
+}
+
+export default About
