b5c355a88eac46579df3ed8c9fac6da4eac149cb
MaiiZuko <maiaramzucco@gmail.com>
2024-03-04 15:11:33 -0300

    update

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
+    const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+    const count = useRef(0)
+
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
+
+    count.current++
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Exercicio 1" />
+            <div className="center">
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{value1} [</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <input type="text" className="input"
+                    ref={myInput1}
+                    value={value1} onChange={e => setValue1(e.target.value)} />
+            </div>
+            <SectionTitle title="Exercicio 1" />
+            <div className="center"></div>
+            <span className="text">Valor: </span>
+            <span className="text">{value2} </span>
+            <input type="text" className="input"
+                value={value2} onChange={e => setValue2(e.target.value)} />
         </div>
     )
 }
