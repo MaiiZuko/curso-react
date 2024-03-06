35723e0f81f08893b3d69741762f556310d199f5
MaiiZuko <maiaramzucco@gmail.com>
2024-03-06 14:55:23 -0300

    useReducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    number: 0
+}
+
+//o objetivo da função reducer é pegar o estado atual e pra cada ação q acontece vai evoluir o estado, alterando um atributo
+function reducer(state, action) {
+    //toda ação tem pelo menos o atributo tipo
+    switch(action.type) {
+        case 'add2ToNumber':
+            return {...state, number: state.number +2}
+        default:
+            //se não ele vai retornar o estado atual
+            return state
+    }
+}
 const UseReducer = (props) => {
     return (
         <div className="UseReducer">
