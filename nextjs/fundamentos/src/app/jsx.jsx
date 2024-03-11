3d570ec7a89dbdcefbbd8c4e71f050a383d8fa01
MaiiZuko <maiaramzucco@gmail.com>
2024-03-11 13:50:02 -0300

    update

---
+++ nextjs/fundamentos/src/app/jsx.jsx
@@ -0,0 +1,12 @@
+// pages/minha-nova-pagina/page.js
+
+import React from 'react';
+
+export default function MinhaNovaPagina() {
+  return (
+    <div>
+      <h1>Minha nova página</h1>
+      <p>Esta é a minha nova página.</p>
+    </div>
+  );
+}
